0cf076bcb87a4f529b1b8a202fec37ccf417e1b6
Ronaldojgc9 <113714316+Ronaldojgc9@users.noreply.github.com>
2022-09-20 12:55:42 -0400

    Merge branch 'main' of https://github.com/Weegee5859/it_cap

--- html/shop.php
+++ html/shop.php
@@ -22,16 +22,32 @@
       <p class="shopTitle"><mark class="highlight">Shop</mark> for the <br> latest trendy styles.</p>
   </div>
 
-  <div class="productList">
-    <div class="product">
-      <img class="productImage" src="resources/shirt.jpg">
-      <div class="productInfoContainer">
-        <p class="productName">Black Shirt</p>
-        <p class="productDesc">Men's Shirt</p>
-        <p class="productPrice">$15.99</p>
-        <div class="productAddCart">Add to Cart</div> 
-      </div>
+  <div class="bar">
+    <div class="icons">
+      <a href="shop.php"><img class="login-icon" src="resources/login.png" alt="logo-icon" width="30px" height="30px"></a>
+      <a href="shop.php"><img class="cart-icon" src="resources/cart.png" alt="cart-icon" width="30px" height="30px"></a>
     </div>
+    <div class="home-title">
+      <p>HOME</p>
+    </div>
+  </div>
+
+  <div class="productList">
+    <div class="navbar">
+        <a href="shop.php">Pants</a>
+        <a href="shop.php">Shirts</a>
+        <a href="shop.php">Sweaters</a>
+        <a href="shop.php">Shorts</a>
+    </div>
+    <div class="product">
+        <img class="productImage" src="resources/shirt.jpg">
+        <div class="productInfoContainer">
+          <p class="productName">Black Shirt</p>
+          <p class="productDesc">Men's Shirt</p>
+          <p class="productPrice">$15.99</p>
+          <div class="productAddCart">Add to Cart</div> 
+        </div>
+      </div>
   </div>
 
   <?php include 'footer.php';?>
