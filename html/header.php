276cb43a29dc52d5baa9c9178add2c917717da89
Weegee5859 <weegee5859@gmail.com>
2022-09-21 17:53:01 -0400

    Updated Header and Shop Navbar
    css for the shops headbar is updated
    
    css icons moved to header.php
    
    minor updates to css for shops product cards

--- html/header.php
+++ html/header.php
@@ -16,9 +16,14 @@
 
 <body>
   <div class="header">
-    <div href="#about">About</div>
-    <div href="#home">Home</div>
-    <div href="#shop">Shopping</div>
+    <div class="label" href="#about">About</div>
+    <div class="label" href="#home">Home</div>
+    <div class="label" href="#shop">Shopping</div>
+    <div class="iconContainer">
+      <div href="shop.php"><img class="login-icon" src="resources/login.png" alt="logo-icon" width="30px" height="30px"></div>
+    <div href="shop.php"><img class="cart-icon" src="resources/cart.png" alt="cart-icon" width="30px" height="30px"></div>
+    </div>
+    
   </div>
 </body>
 </html>
--- html/shop.php
+++ html/shop.php
@@ -21,33 +21,34 @@
       <img class="shopImage" src="resources/clothes2.jpg">
       <p class="shopTitle"><mark class="highlight">Shop</mark> for the <br> latest trendy styles.</p>
   </div>
-
-  <div class="bar">
-    <div class="icons">
-      <a href="shop.php"><img class="login-icon" src="resources/login.png" alt="logo-icon" width="30px" height="30px"></a>
-      <a href="shop.php"><img class="cart-icon" src="resources/cart.png" alt="cart-icon" width="30px" height="30px"></a>
-    </div>
-    <div class="home-title">
-      <p>HOME</p>
-    </div>
-  </div>
-
-  <div class="productList">
+  <div class="mainContainer">
     <div class="navbar">
-        <a href="shop.php">Pants</a>
-        <a href="shop.php">Shirts</a>
-        <a href="shop.php">Sweaters</a>
-        <a href="shop.php">Shorts</a>
+      <div href="shop.php">Pants</div>
+      <div href="shop.php">Shirts</div>
+      <div href="shop.php">Sweaters</div>
+      <div href="shop.php">Shorts</div>
     </div>
-    <div class="product">
-        <img class="productImage" src="resources/shirt.jpg">
-        <div class="productInfoContainer">
-          <p class="productName">Black Shirt</p>
-          <p class="productDesc">Men's Shirt</p>
-          <p class="productPrice">$15.99</p>
-          <div class="productAddCart">Add to Cart</div> 
+    <div class="productList">
+      <div class="product">
+          <img class="productImage" src="resources/shirt.jpg">
+          <div class="productInfoContainer">
+            <p class="productName">Black Shirt</p>
+            <p class="productDesc">Men's Shirt</p>
+            <p class="productPrice">$15.99</p>
+            <div class="productAddCart">Add to Cart</div> 
+          </div>
         </div>
-      </div>
+
+        <div class="product">
+          <img class="productImage" src="resources/shirt.jpg">
+          <div class="productInfoContainer">
+            <p class="productName">Black Shirt</p>
+            <p class="productDesc">Men's Shirt</p>
+            <p class="productPrice">$15.99</p>
+            <div class="productAddCart">Add to Cart</div> 
+          </div>
+        </div>
+    </div>
   </div>
 
   <?php include 'footer.php';?>
